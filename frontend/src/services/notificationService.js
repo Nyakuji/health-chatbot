3c0e0e7d1cadd819145fb769cfec5a1316592c91
philip Opiyo <opiyophilipo@gmail.com>
2024-06-15 16:34:23 +0300

    feat: Add notification service for sending appointment reminders

---
+++ frontend/src/services/notificationService.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api/notification/';
+
+const sendReminder = async (appointmentId, contactInfo, notificationType) => {
+  const response = await axios.post(`${API_URL}send-reminder`, { appointmentId, contactInfo, notificationType });
+  return response.data;
+};
+
+const notificationService = { sendReminder };
+
+export default notificationService;
