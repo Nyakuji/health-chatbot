71836b29b3a6462e095902c8f3e5aa8d308e55bd
philip Opiyo <opiyophilipo@gmail.com>
2024-07-03 20:37:28 +0300

    feat: Add getAppointmentHistory route
    This commit adds the getAppointmentHistory route to the appointmentRoutes.js file. The route allows users to retrieve their appointment history. This feature enhances the functionality of the application by providing users with access to their past appointments.

--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -3,11 +3,16 @@ const {
   bookAppointment,
   getDoctorAvailability,
   cancelAppointment,
+  getAppointmentHistory
 } = require('../controllers/appointmentController')
+const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router()
 
-router.post('/book', bookAppointment)
+router.use(authMiddleware)
+
 router.get('/availability', getDoctorAvailability)
+router.post('/book', bookAppointment)
 router.post('/cancel', cancelAppointment)
+router.get('/history', getAppointmentHistory);
 
 module.exports = router
