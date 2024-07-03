dabc9609fde17977b7336df5b94a762fd78b587f
philip Opiyo <opiyophilipo@gmail.com>
2024-07-03 20:38:32 +0300

    feat: Update profile picture upload and update logic
    This commit updates the profileController.js file to improve the logic for uploading and updating profile pictures. It fixes a bug where the user was not found when uploading a profile picture and adds validation to ensure that a profile picture file is required. Additionally, it returns appropriate JSON responses with status codes and messages for successful and error scenarios. These changes enhance the functionality and user experience of the profile picture feature.

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -1,11 +1,11 @@
 const User = require('../models/userModel')
-const Appointment = require('../models/appointmentModel')
 const multer = require('multer')
+const path = require('path')
 
 // Set up multer for file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/')
+    cb(null, path.join(__dirname, '../uploads/')) //eslint-disable-line
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`)
@@ -14,25 +14,37 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage })
 
+// Middleware to handle file upload and profile picture update
 exports.uploadProfilePicture = [
   upload.single('profilePicture'),
   async (req, res) => {
     try {
       const user = await User.findById(req.userId)
       if (!user) {
-        return res.status(404).send('User not found')
+        return res.status(404).json({ message: 'User not found' })
+      }
+
+      if (!req.file) {
+        return res
+          .status(400)
+          .json({ message: 'Profile picture file is required' })
       }
 
       user.profilePicture = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`
       await user.save()
 
-      res.send(user)
+      res
+        .status(200)
+        .json({ message: 'Profile picture updated successfully', user })
     } catch (error) {
-      res.status(400).send(error.message)
+      res
+        .status(500)
+        .json({ message: 'Internal server error', error: error.message })
     }
   },
 ]
 
+// Controller to update user profile
 exports.updateProfile = async (req, res) => {
   const { userId, username, email, phoneNumber, medicalId } = req.body
 
@@ -52,33 +64,8 @@ exports.updateProfile = async (req, res) => {
 
     res.status(200).json({ message: 'Profile updated successfully', user })
   } catch (error) {
-    res.status(500).json({ message: 'Internal server error', error })
-  }
-}
-
-exports.getAppointmentHistory = async (req, res) => {
-  const { userId } = req.query
-
-  try {
-    const user = await User.findById(userId)
-
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' })
-    }
-
-    let appointments
-    if (user.role === 'doctor') {
-      appointments = await Appointment.find({ doctorId: userId })
-        .populate('doctorId')
-        .populate('patientId')
-    } else {
-      appointments = await Appointment.find({ patientId: userId })
-        .populate('doctorId')
-        .populate('patientId')
-    }
-
-    res.status(200).json({ appointments })
-  } catch (error) {
-    res.status(500).json({ message: 'Internal server error', error })
+    res
+      .status(500)
+      .json({ message: 'Internal server error', error: error.message })
   }
 }
