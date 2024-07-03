68407ff5f059d1185f8b1fcedd3d9d2019903492
philip Opiyo <opiyophilipo@gmail.com>
2024-07-03 11:16:27 +0300

    chore: lint fix

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,20 +1,22 @@
 module.exports = {
-    testEnvironment: 'node',
-    setupFilesAfterEnv: ['./tests/setup.js'],
-    testPathIgnorePatterns: ['/node_modules/'],
-    transform: {
-      '^.+\\.js$': 'babel-jest',
-    },
-    transformIgnorePatterns: [
-      'node_modules/(?!(chai)/)', // Transform all node_modules except chai
-    ],
-    globals: {
-      'ts-jest': {
-        diagnostics: {
-          warnOnly: true,
-        },
+  testTimeout: 30000, // Set the test timeout to 30 seconds
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['./tests/setup.js'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(chai)/)', // Transform all node_modules except chai
+  ],
+  globals: {
+    'ts-jest': {
+      diagnostics: {
+        warnOnly: true,
       },
     },
-    verbose: true, // Enable verbose output
-  }
-  
+  },
+  verbose: true, // Enable verbose output
+}
--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,13 @@
-const path = require('path')
 const process = require('process')
-const express = require('express')
-const mongoose = require('mongoose')
 const http = require('http')
-const cors = require('cors')
 const { wss } = require('./websocket')
-const routes = require('./routes')
 const { initSocket } = require('./socket')
+const app = require('./app')
 
 const PORT = process.env.PORT || 5000
 
-require('dotenv').config()
-
-const app = express()
-const server = http.createServer((req, res) => {
-  app(req, res)
-})
+const server = http.createServer(app)
 initSocket(server)
-
-// Middleware
-app.use(cors())
-app.use(express.json())
-
-// Use the consolidated routes
-app.use('/api', routes)
-
-// Disable ESLint no-undef rule for the next line
-// eslint-disable-next-line no-undef
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
-
-// MongoDB connection
-mongoose
-  .connect(process.env.MONGODB_URI ?? 'mongodb://localhost/mydatabase')
-  .then(() => {
-    console.info('Connected to MongoDB')
-  })
-  .catch((err) => {
-    console.error('Database connection error:', err)
-  })
 
 server.on('upgrade', (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, (ws) => {
