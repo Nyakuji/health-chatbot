3446372d86b5db62b467ad42bd5e8665d7ebbdd6
philip Opiyo <opiyophilipo@gmail.com>
2024-07-05 00:57:44 +0300

    refactor: Update profileService.js to improve code structure and error handling
    This commit refactors the profileService.js file to improve code structure and error handling. It separates the getUser, updateProfile, and uploadProfilePicture functions into individual exports for better organization. It also adds try-catch blocks to handle errors during API requests and displays appropriate error messages. This refactoring enhances the readability and maintainability of the code.

--- frontend/src/services/profileService.js
+++ frontend/src/services/profileService.js
@@ -1,37 +1,59 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:5000/api/profile/'
+const API_URL = 'http://localhost:5000/api/profile'
 
-const updateProfile = async (profileData) => {
-  const response = await axios.post(`${API_URL}update`, profileData)
-  return response.data
-}
-
+// Fetch user data by user ID
 const getUser = async (userId) => {
-  const response = await axios.get(`${API_URL}${userId}`, {
-    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-  })
-  return response.data
+  try {
+    const response = await axios.get(`${API_URL}/${userId}`)
+    return response.data
+  } catch (error) {
+    console.error(
+      'Error fetching user data:',
+      error.response ? error.response.data : error.message
+    )
+    throw error
+  }
 }
 
+// Update user profile
+const updateProfile = async (profileData) => {
+  try {
+    const response = await axios.post(`${API_URL}/update`, profileData)
+    return response.data
+  } catch (error) {
+    console.error(
+      'Error updating profile:',
+      error.response ? error.response.data : error.message
+    )
+    throw error
+  }
+}
+
+// Upload profile picture
 const uploadProfilePicture = async (formData) => {
-  const response = await axios.post(
-    `${API_URL}uploadProfilePicture`,
-    formData,
-    {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
-    }
-  )
-  return response.data
+  try {
+    const response = await axios.post(
+      `${API_URL}/uploadProfilePicture`,
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      }
+    )
+    return response.data
+  } catch (error) {
+    console.error(
+      'Error uploading profile picture:',
+      error.response ? error.response.data : error.message
+    )
+    throw error
+  }
 }
 
-const profileService = {
-  updateProfile,
+export default {
   getUser,
+  updateProfile,
   uploadProfilePicture,
 }
-
-export default profileService
