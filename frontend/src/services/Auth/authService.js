0f70270571000db116bac75940082cb3e70be62f
philip Opiyo <opiyophilipo@gmail.com>
2024-07-06 15:46:06 +0300

    feat: Add authService module for user authentication
    This commit adds the authService module, which provides functions for user authentication such as signup, login, logout, and getCurrentUser. The module utilizes the axios library to make HTTP requests to the backend API. This feature enhances the frontend by enabling users to sign up, log in, and manage their authentication state.

---
+++ frontend/src/services/Auth/authService.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api/';
+
+const signup = async (userData) => {
+  const response = await axios.post(`${API_URL}signup`, userData);
+  if (response.data.token) {
+    localStorage.setItem('user', JSON.stringify(response.data));
+  }
+  return response.data;
+};
+
+const login = async (userData) => {
+  const response = await axios.post(`${API_URL}login`, userData);
+  if (response.data.token) {
+    localStorage.setItem('user', JSON.stringify(response.data));
+  }
+  return response.data;
+};
+
+const logout = () => {
+  localStorage.removeItem('user');
+};
+
+const getCurrentUser = () => {
+  const user = localStorage.getItem('user');
+  return user ? JSON.parse(user) : null;
+};
+
+const authService = {
+  signup,
+  login,
+  logout,
+  getCurrentUser,
+};
+
+export default authService;
