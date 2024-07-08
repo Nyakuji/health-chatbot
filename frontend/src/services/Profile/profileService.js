c602c09730c9990c81f6a5a1992e95f7923dc614
philip Opiyo <opiyophilipo@gmail.com>
2024-07-08 13:50:40 +0300

    refactor: Update profileService.js to include authorization headers
    This commit updates the profileService.js file to include authorization headers in the requests made to the API. The headers are obtained from the authService.getToken() function and added to the axios requests using the 'Authorization' key. This change ensures that the user's token is sent along with the requests, allowing for proper authentication and authorization.

--- frontend/src/services/Profile/profileService.js
+++ frontend/src/services/Profile/profileService.js
@@ -1,23 +1,36 @@
 import axios from 'axios'
+import authService from '../Auth/authService'
 
 const API_URL = 'http://localhost:5000/api/profile/'
 
 const getProfile = async (userId) => {
-  const response = await axios.get(`${API_URL}${userId}`)
+  const token = authService.getToken()
+  const response = await axios.get(`${API_URL}${userId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
   return response.data
 }
 
 const updateProfile = async (profileData) => {
-  const response = await axios.post(`${API_URL}update`, profileData)
+  const token = authService.getToken()
+  const response = await axios.post(`${API_URL}update`, profileData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
   return response.data
 }
 
 const uploadProfilePicture = async (formData) => {
+  const token = authService.getToken()
   const response = await axios.post(
     `${API_URL}uploadProfilePicture`,
     formData,
     {
       headers: {
+        Authorization: `Bearer ${token}`,
         'Content-Type': 'multipart/form-data',
       },
     }
