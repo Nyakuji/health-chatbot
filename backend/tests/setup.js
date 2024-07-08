75779e0443ca63683004e3a531da5c1db52d1d61
philip Opiyo <opiyophilipo@gmail.com>
2024-07-08 13:55:09 +0300

    refactor: Update setup.js to improve test setup and teardown
    This commit updates the setup.js file in the backend/tests directory to improve the setup and teardown process for tests. It includes the following changes:
    - Added sinon library for stubbing and restoring functions
    - Modified the beforeAll hook to disconnect from the MongoDB server if a connection already exists
    - Added a check to stop the MongoDB server only if it is running
    - Modified the beforeEach hook to use the deleteMany() method directly on the collection objects
    - Added a sinon.restore() call in the afterEach hook to restore any stubbed functions
    
    These changes enhance the test setup and teardown process, ensuring a clean and reliable testing environment.

--- backend/tests/setup.js
+++ backend/tests/setup.js
@@ -1,31 +1,38 @@
 const mongoose = require('mongoose')
-const chai = require('chai')
 const { MongoMemoryServer } = require('mongodb-memory-server')
-const { beforeAll, afterAll, afterEach } = require('@jest/globals')
-require('dotenv').config()
+const chai = require('chai')
+const sinon = require('sinon')
+const { beforeEach, afterEach, beforeAll, afterAll } = require('@jest/globals')
 
-chai.should()
+require('dotenv').config()
 
 let mongoServer
 
 beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create()
   const mongoUri = mongoServer.getUri()
+  if (mongoose.connection.readyState !== 0) {
+    await mongoose.disconnect()
+  }
   await mongoose.connect(mongoUri)
 })
 
 afterAll(async () => {
   await mongoose.disconnect()
-  await mongoServer.stop()
-})
-
-afterEach(async () => {
-  const collections = mongoose.connection.collections
-
-  for (const key in collections) {
-    const collection = collections[key]
-    await collection.deleteMany({})
+  if (mongoServer) {
+    await mongoServer.stop()
   }
 })
 
-module.exports = chai
+beforeEach(async () => {
+  const collections = mongoose.connection.collections
+  for (const key in collections) {
+    await collections[key].deleteMany()
+  }
+})
+
+afterEach(() => {
+  sinon.restore()
+})
+
+chai.should()
