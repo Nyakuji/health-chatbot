adfdd6daffb644bfadbeb53c8c41bbfe2b183000
philip Opiyo <opiyophilipo@gmail.com>
2024-07-18 09:56:16 +0300

    refactor: Update feedbackRoutes to remove unnecessary middleware
    This commit refactors the feedbackRoutes file to remove the unnecessary middleware functions verifyToken and isPatient. These middleware functions were previously used to authenticate and authorize the user, but they are no longer needed since the authentication and authorization logic has been moved to the createFeedback and getFeedbackForDoctor functions in the feedbackController.
    
    This change simplifies the code and improves the maintainability of the feedbackRoutes file.

--- backend/routes/feedbackRoutes.js
+++ backend/routes/feedbackRoutes.js
@@ -2,11 +2,10 @@ const {
   createFeedback,
   getFeedbackForDoctor,
 } = require('../controllers/feedbackController')
-const { verifyToken, isPatient } = require('../middleware/authMiddleware')
 
 const feedbackRoutes = (app) => {
-  app.post('/api/feedback', verifyToken, isPatient, createFeedback)
-  app.get('/api/feedback/:doctorId', verifyToken, getFeedbackForDoctor)
+  app.post('/api/feedback', createFeedback)
+  app.get('/api/feedback/:doctorId', getFeedbackForDoctor)
 }
 
 module.exports = feedbackRoutes
